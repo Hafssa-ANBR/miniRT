b8188550ac317f5d29bd3ff48d7fd233679b0f71
haffsa pc <hafssa.2017m5@gmil.com>
2025-04-05 11:21:41 -0700

    push

--- ff.cpp
+++ ff.cpp
@@ -1,5 +1,7 @@
 Point operator+(const Point& p) {
         return Point(x + p.x, y + p.y);
     }
+
+    
 //Surcharge des opérateurs (Operator Overloading)
 //Surcharge de fonctions (Function Overloading)
